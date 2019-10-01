5c6bccca0de00434b356ff4c3324d42420998dd5
Hugo Falci <hugo.f.ribeiro@outlook.com>
2019-10-01 19:46:00 -0300

    Commit 1 da aula 2

--- aula02/Form1.Designer.cs
+++ aula02/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxDias = new System.Windows.Forms.ComboBox();
+            this.cbxMeses = new System.Windows.Forms.ComboBox();
+            this.cbxAnos = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textPeso = new System.Windows.Forms.TextBox();
+            this.textAltura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(84, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(261, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nascimento";
+            // 
+            // cbxDias
+            // 
+            this.cbxDias.FormattingEnabled = true;
+            this.cbxDias.Location = new System.Drawing.Point(84, 53);
+            this.cbxDias.Name = "cbxDias";
+            this.cbxDias.Size = new System.Drawing.Size(48, 21);
+            this.cbxDias.TabIndex = 3;
+            // 
+            // cbxMeses
+            // 
+            this.cbxMeses.FormattingEnabled = true;
+            this.cbxMeses.Location = new System.Drawing.Point(138, 53);
+            this.cbxMeses.Name = "cbxMeses";
+            this.cbxMeses.Size = new System.Drawing.Size(84, 21);
+            this.cbxMeses.TabIndex = 4;
+            // 
+            // cbxAnos
+            // 
+            this.cbxAnos.FormattingEnabled = true;
+            this.cbxAnos.Location = new System.Drawing.Point(228, 53);
+            this.cbxAnos.Name = "cbxAnos";
+            this.cbxAnos.Size = new System.Drawing.Size(117, 21);
+            this.cbxAnos.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Peso";
+            // 
+            // textPeso
+            // 
+            this.textPeso.Location = new System.Drawing.Point(84, 85);
+            this.textPeso.Name = "textPeso";
+            this.textPeso.Size = new System.Drawing.Size(75, 20);
+            this.textPeso.TabIndex = 7;
+            // 
+            // textAltura
+            // 
+            this.textAltura.Location = new System.Drawing.Point(84, 111);
+            this.textAltura.Name = "textAltura";
+            this.textAltura.Size = new System.Drawing.Size(75, 20);
+            this.textAltura.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Altura";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(84, 153);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 46);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "CalcularIMC";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textAltura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPeso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxAnos);
+            this.Controls.Add(this.cbxMeses);
+            this.Controls.Add(this.cbxDias);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxDias;
+        private System.Windows.Forms.ComboBox cbxMeses;
+        private System.Windows.Forms.ComboBox cbxAnos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textPeso;
+        private System.Windows.Forms.TextBox textAltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
     }
 }
 
--- aula02/Form1.cs
+++ aula02/Form1.cs
@@ -12,9 +12,72 @@ namespace aula02
 {
     public partial class Form1 : Form
     {
+        private decimal altura;
+        private decimal peso;
+        private int[] dias;
+        private List<int> anos;
+        private Dictionary<int, string> meses;
+
         public Form1()
         {
             InitializeComponent();
+
+            dias = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
+            anos = new List<int>()
+            {
+                1994,
+                1995,
+                1996,
+                1997,
+                1998,
+                1999,
+                2000,
+                2001,
+                2002,
+                2003
+            };
+
+            anos.Add(2004);
+
+            meses = new Dictionary<int, string>()
+            {
+                {1, "Janeiro" },
+                {2, "Fevereiro" },
+                {3, "Março" },
+                {4, "Abril" },
+                {5, "Maio" },
+                {6, "Junho" },
+                {7, "Julho" },
+                {8, "Agosto" },
+                {9, "Setembro" },
+                {10, "Outubro" },
+                {11, "Novembro" },
+                {12, "Dezembro" }
+            };
+
+            int indice = 0;
+            while(indice < dias.Length)
+            {
+                cbxDias.Items.Add(dias[indice]);
+                indice++;
+            }
+            for(int i = 0; i < dias.Length; i++)
+            {
+                cbxDias.Items.Add(dias[i]);
+            }
+            //
+            foreach(int ano in anos)
+            {
+                cbxAnos.Items.Add(ano);
+            }
+
+            //
+            cbxMeses.DataSource = new BindingSource(meses, null);
+            //
+            cbxMeses.DisplayMember = "Value";
+            //
+            cbxMeses.ValueMember = "Key";
         }
     }
 }
